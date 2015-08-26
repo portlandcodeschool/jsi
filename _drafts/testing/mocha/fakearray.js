7ee925a30c0690031167274e1c39f98a21bdff66
Tom McCluskey <bosun.tom@gmail.com>
2015-08-26 00:51:18 -0700

    added page for 3rd Wednesday (mocha/chai) and a sample file to be tested and test

---
+++ _drafts/testing/mocha/fakearray.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  length: 0,
+  pop: function() {
+    var returnValue = this[this.length - 1];
+    this[this.length-1] = undefined;
+    this.length--;
+    return returnValue;
+  },
+  push: function(input) {
+    this[this.length] = input;
+    this.length++;
+  }
